cc313d2fbb0b1983216f8cfd747bde95b744b99b
Christopher Robertson <christopherprobertson@gmail.com>
2019-11-12 11:40:38 +0000

    Created a new script called UISound.cs which at the moment will serve as the main class for all of the UI elements when giving feedback to the player, but I might end up making it the parent class and then a button child class, a slider one, and so on, since I might want them to interact in different ways, but in the same context. I also added the SoundSettings struct to store all of the sound data since I found myself repeating the same variables in a variety of scripts, so having a struct to store them all is incredibly helpful. A visual identifier of SpawnSampleSquare(...) was created to see the wave rather than just hear it. I also started some docstring notation on my code in the XML C# format.

--- tinkering_audio/Assets/tinkering_audio/Scripts/ToneGenerator.cs
+++ tinkering_audio/Assets/tinkering_audio/Scripts/ToneGenerator.cs
@@ -1,41 +1,108 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct SoundSettings
+{
+    public int sampleRate;
+    public int sampleDurationSecs;
+    [HideInInspector]
+    public int sampleLength;
+}
 
 [RequireComponent(typeof(AudioSource))]
 public class ToneGenerator : MonoBehaviour
 {
+    [Header("Singleton Referencing")]
+    public static ToneGenerator Instance;
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(this.gameObject);
+    }
+
     [Header("Wave Attributes")]
     [SerializeField]
-    private int sampleRate = 44100;
-    [SerializeField]
-    private int sampleDurationSecs = 5;
-    private int sampleLength;
-
+    private SoundSettings soundSettings;
     private AudioSource audioSource;
+
+    public List<float> samplesList = new List<float>();
+    public GameObject squarePrefab;
 
     private void Start()
     {
-        sampleLength = sampleRate * sampleDurationSecs;
+        soundSettings.sampleLength = soundSettings.sampleRate * soundSettings.sampleDurationSecs;
 
         audioSource = this.GetComponent<AudioSource>();
-        AudioClip newClip = CreateToneAudioClip(1500);
-        audioSource.PlayOneShot(newClip);
+        AudioClip newClip = CreateToneAudioClip(soundSettings, 1500);
     }
 
-    private AudioClip CreateToneAudioClip(int frequency)
+
+    /// <summary>
+    /// A function that uses a variety of sound settings and 
+    /// the frequency to generate a clip of sound
+    /// </summary>
+    /// <param name="soundSettings"></param>
+    /// <param name="frequency"></param>
+    /// <returns>
+    /// A Unity AudioClip data type
+    /// </returns>
+    public AudioClip CreateToneAudioClip(SoundSettings soundSettings, int frequency)
     {
+        soundSettings.sampleLength = soundSettings.sampleRate * soundSettings.sampleDurationSecs;
         float maxValue = 1f / 4f;
 
-        AudioClip audioClip = AudioClip.Create("new_tone", sampleLength, 1, sampleRate, false);
+        AudioClip audioClip = AudioClip.Create("new_tone", soundSettings.sampleLength, 1, soundSettings.sampleRate, false);
 
-        float[] samples = new float[sampleLength];
-        for (var i = 0; i < sampleLength; i++)
+        float[] samples = new float[soundSettings.sampleLength];
+        for (var i = 0; i < soundSettings.sampleLength; i++)
         {
-            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float s = GetSinValue(frequency, i, soundSettings.sampleRate);
             float v = s * maxValue;
             samples[i] = v;
+            
+
         }
+
+        SpawnSampleSquare(samples, 200);
 
         audioClip.SetData(samples, 0);
         return audioClip;
+    }
+
+    /// <summary>
+    /// Uses the samples from the audioclip to generate a visual intepretation of the wave using squares.
+    /// </summary>
+    /// <remarks>
+    /// It can and should be limited otherwise it will cause peformance issues.
+    /// </remarks>
+    /// <param name="samples"></param>
+    /// <param name="amountToSpawn"</param>
+    private void SpawnSampleSquare(float[] samples, int amountToSpawn)
+    {
+        if (amountToSpawn > samples.Length - 1) amountToSpawn = samples.Length - 1;
+
+        Vector3 spawnPosition = Vector3.zero;
+        float squareIntervals = 2.5f;
+
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            spawnPosition = new Vector3(spawnPosition.x + squareIntervals, samples[i] * 100 , 0);
+            Instantiate(squarePrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    /// <summary>
+    ///  A sine function that produces a sine wave based on
+    ///  a variety of variables
+    /// </summary>
+    /// <param name="frequency"></param>
+    /// <param name="indexPosition"></param>
+    /// <param name="sampleRate"></param>
+    /// <returns></returns>
+    private float GetSinValue(float frequency, float indexPosition, float sampleRate)
+    {
+        return Mathf.Sin(2.0f * Mathf.PI * frequency * (indexPosition / sampleRate));
     }
 }
---
+++ tinkering_audio/Assets/tinkering_audio/Scripts/UISound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UISound : MonoBehaviour
+{
+    [SerializeField]
+    private SoundSettings soundSettings;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        if (this.GetComponent<AudioSource>() == null)
+        {
+            audioSource = this.gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        }
+
+        else
+        {
+            audioSource = this.GetComponent<AudioSource>();
+        }
+    }
+}
