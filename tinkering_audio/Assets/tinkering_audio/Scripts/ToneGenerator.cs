0722ba01f758d4e4d2974fe3afe4d6ceb2a4663b
Christopher Robertson <christopherprobertson@gmail.com>
2019-11-12 10:33:26 +0000

    Implemented a base ToneGenerator.cs using the workshop materials which now allows for a simple sound to be produced given a variety of variables. I also renamed the prefabs and materials to have their extension name after rather than before; e.g: PRF_Cube.prefab is now Cube_PRF.prefab. I also added a default Audio Mixer as well as another scene for my contract specifically (Contract 4)

---
+++ tinkering_audio/Assets/tinkering_audio/Scripts/ToneGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ToneGenerator : MonoBehaviour
+{
+    [Header("Wave Attributes")]
+    [SerializeField]
+    private int sampleRate = 44100;
+    [SerializeField]
+    private int sampleDurationSecs = 5;
+    private int sampleLength;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        sampleLength = sampleRate * sampleDurationSecs;
+
+        audioSource = this.GetComponent<AudioSource>();
+        AudioClip newClip = CreateToneAudioClip(1500);
+        audioSource.PlayOneShot(newClip);
+    }
+
+    private AudioClip CreateToneAudioClip(int frequency)
+    {
+        float maxValue = 1f / 4f;
+
+        AudioClip audioClip = AudioClip.Create("new_tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+}
